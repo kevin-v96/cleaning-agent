51c57d828c0e0ce494de8b9f221fa58d192b271d
kevin-v96 <kevinvegda@gmail.com>
2024-08-01 13:40:47 +0530

    update pre-commit-config

--- agents.py
+++ agents.py
@@ -19,9 +19,11 @@ class CleaningServiceAgents:
 			redirect them to the Customer Service Assistant. If they ask for anything else, \
 		 tell them that you can only help with cleaning services."
             ),
-            backstory=dedent("""\
-				As a Cleaning Guardrail Agent for a Home Services Company, you are adept at listening 
-					to customer queries, and figuring out if their queries are related to cleaning services."""),
+            backstory=dedent(
+                """\
+				As a Cleaning Guardrail Agent for a Home Services Company, you are adept at listening
+					to customer queries, and figuring out if their queries are related to cleaning services."""
+            ),
             verbose=True,
             allow_delegation=True,
         )
@@ -30,15 +32,17 @@ class CleaningServiceAgents:
         return Agent(
             role="Customer Service Assistant",
             goal="To redirect customers to the right agent. If the customer asks for cleaning related services, running SQL queries to find out the next availability for the required cleaning service",
-            backstory=dedent("""\
-				As a Customer Service Assistant for a Home Services Company, you are adept at listenint 
-				to customer queries, figuring out what services they need and how to get them the service. 
+            backstory=dedent(
+                """\
+				As a Customer Service Assistant for a Home Services Company, you are adept at listenint
+				to customer queries, figuring out what services they need and how to get them the service.
 				You listen with empathy, figure out the customer needs, and if they need cleaning services, tell them the next availability.
                 You do this by running SQL queries to get the availability of the cleaning staff and the customer's preferred time.
 				Connect to the local SQLite DB at connection string = availabilities.db
-            	Try to analyze the tables first by listing all the tables and columns 
-            	and doing distinct values for each column and once sure, make a query to 
-            	get the data you need."""),
+            	Try to analyze the tables first by listing all the tables and columns
+            	and doing distinct values for each column and once sure, make a query to
+            	get the data you need."""
+            ),
             verbose=True,
             allow_delegation=False,
             tools=sql_tools,
--- database.py
+++ database.py
@@ -1,25 +1,29 @@
 import sqlite3
 
-conn = sqlite3.connect('availabilities.db')
+conn = sqlite3.connect("availabilities.db")
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS availabilities (
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS availabilities (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     service TEXT NOT NULL,
-    next_availability DATETIME NOT NULL)''')
+    next_availability DATETIME NOT NULL)"""
+)
 
 employees = [
-    ('John Doe', 'House Cleaning', '2024-08-01 08:00:00'),
-    ('Jane Doe', 'Apartment Cleaning', '2024-08-03 14:00:00'),
-    ('John Smith', 'Plumbing', '2024-08-04 08:00:00')
+    ("John Doe", "House Cleaning", "2024-08-01 08:00:00"),
+    ("Jane Doe", "Apartment Cleaning", "2024-08-03 14:00:00"),
+    ("John Smith", "Plumbing", "2024-08-04 08:00:00"),
 ]
 
 # Insert data into the 'employee' table
-insert_query = 'INSERT INTO availabilities (name, service, next_availability) VALUES (?, ?, ?);'
+insert_query = (
+    "INSERT INTO availabilities (name, service, next_availability) VALUES (?, ?, ?);"
+)
 cursor.executemany(insert_query, employees)
 
-print(cursor.execute('SELECT * FROM availabilities').fetchall())
+print(cursor.execute("SELECT * FROM availabilities").fetchall())
 
 # Commit the changes
 conn.commit()
