c25476ac25ba0892a459b6ab46d78403121ce8cb
kevin-v96 <kevinvegda@gmail.com>
2024-08-04 07:55:26 +0100

    add langserve playground

--- graph.py
+++ graph.py
@@ -1,10 +1,9 @@
 # env
 from dotenv import load_dotenv
 
-import uuid
 
 # langgraph
-from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
 from langgraph.graph import END, StateGraph, START
 from langchain_openai import ChatOpenAI
 
@@ -34,15 +33,44 @@ from nodes import (
     route_to_workflow,
 )
 
+# fastapi
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langchain_core.pydantic_v1 import BaseModel
+from langserve import add_routes, RemoteRunnable
+import uvicorn
+
 # chat
-from chat import run_chat
 
 # utils
 from utils import create_tool_node_with_fallback, user_id
+from pprint import pprint
 
 load_dotenv()
 
 db = "availabilities.sqlite"
+
+
+class Input(BaseModel):
+    input: str
+
+
+class Output(BaseModel):
+    output: dict
+
+
+def get_response(input_text):
+    app = RemoteRunnable("http://localhost:8000/superbench_chat/")
+    for output in app.stream({"input": input_text}):
+        for key, value in output.items():
+            # Node
+            pprint(f"Node '{key}':")
+            # Optional: print full state at each node
+            # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+        pprint("\n---\n")
+    output = value["generation"]
+    return output
+
 
 if __name__ == "__main__":
     llm = ChatOpenAI(model="gpt-4o-mini", temperature=1)
@@ -101,7 +129,7 @@ if __name__ == "__main__":
     builder.add_conditional_edges("fetch_user_id", route_to_workflow)
 
     # compile graph
-    memory = SqliteSaver.from_conn_string(":memory:")
+    memory = AsyncSqliteSaver.from_conn_string(":memory:")
     graph = builder.compile(
         checkpointer=memory,
         interrupt_before=[
@@ -109,20 +137,53 @@ if __name__ == "__main__":
         ],
     )
 
-    thread_id = str(uuid.uuid4())
+    # thread_id = str(uuid.uuid4())
 
-    config = {
-        "configurable": {
-            # The user_id is used to keep a track of the user we are currently serving. In this case, we are using the thread_id as the user_id
-            "user_id": thread_id,
-            # Checkpoints are accessed by thread_id
-            "thread_id": thread_id,
-        }
-    }
+    # config = {
+    #     "configurable": {
+    #         # The user_id is used to keep a track of the user we are currently serving. In this case, we are using the thread_id as the user_id
+    #         "user_id": thread_id,
+    #         # Checkpoints are accessed by thread_id
+    #         "thread_id": thread_id,
+    #     }
+    # }
 
-    _printed = set()
+    # _printed = set()
 
-    user_input = input(
-        "Hi, I'm a cleaning service booking assistant for Superbench. How can I help you today?\n"
+    # user_input = input(
+    #    "Hi, I'm a cleaning service booking assistant for Superbench. How can I help you today?\n"
+    # )
+    # result = run_chat(graph, user_input, config, _printed)
+
+    app = FastAPI(
+        title="Superbench Server",
+        version="1.0",
+        description="An API server to answer questions about cleaning services",
     )
-    result = run_chat(graph, user_input, config, _printed)
+
+    @app.get("/")
+    async def redirect_root_to_docs():
+        return RedirectResponse("/docs")
+
+    add_routes(
+        app,
+        graph.with_types(input_type=Input, output_type=Output),
+        path="/superbench_chat",
+    )
+
+    uvicorn.run(app, host="localhost", port=8000)
+
+    # # Create the UI In Gradio
+    # iface = gr.Interface(fn=get_response,
+    #         inputs=gr.Textbox(
+    #         value="Enter your question"),
+    #         outputs="textbox",
+    #         title="Q&A bot for Superbench",
+    #         description="Ask a question about booking cleaning services with Superbench.",
+    #         examples=[["What cleaning services do you have available?"],
+    #                 ["When is the next availability for post-renovation cleaning?"],
+    #                 ],
+    #         theme=gr.themes.Soft(),
+    #         allow_flagging="never",)
+
+    # iface.launch() # put share equal to True for public URL
